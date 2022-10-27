edf64b21d214a72a28a80334b26c4691d9711217
darkdevler <romatkachenko3577@gmail.com>
2022-10-27 12:52:37 +0300

    added file task5.py

---
+++ Lesson_2/task5.py
@@ -0,0 +1,9 @@
+enter_list = input("Enter list: ")
+numbers_list = list(map(int, enter_list.split(",")))
+numbers_list.sort()
+
+small_number = numbers_list[0]
+largest_number = numbers_list[-1]
+all_numbers = sum(numbers_list)
+
+print(f"Smallest number: {small_number};\nLargest number: {largest_number};\nSum all numbers: {all_numbers};")
