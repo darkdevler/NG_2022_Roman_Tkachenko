9ab612ffc1c8e0cea91bd658d9df82a6e610efb8
darkdevler <romatkachenko3577@gmail.com>
2022-10-31 16:00:27 +0200

    edit task5.py

--- Lesson_2/task5.py
+++ Lesson_2/task5.py
@@ -4,6 +4,6 @@ numbers_list.sort()
 
 small_number = numbers_list[0]
 largest_number = numbers_list[-1]
-all_numbers = sum(numbers_list)
+all_numbers = sum(numbers_list[1:-1])
 
 print(f"Smallest number: {small_number};\nLargest number: {largest_number};\nSum all numbers: {all_numbers};")
